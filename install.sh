c199072f896380126dd10ecfed3eed38eef75ef2
Hanpeng Liu <lhp.polo@gmail.com>
2019-01-22 19:07:14 -0800

    fix bug

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/usr/bin/python -m pip install git+https://github.com/pinle-go/PDFRename.git --user >/dev/null
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     cp $PWD/batch_pdf_rename.sh $HOME/local/bin/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
