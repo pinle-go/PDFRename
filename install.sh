07847244b91877160f1caff74cf6262c5bda7fc0
Hanpeng Liu <lhp.polo@gmail.com>
2019-01-22 19:00:59 -0800

    fix bug

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     cp $PWD/batch_pdf_rename.sh $HOME/local/bin/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    cp -r Rename PDFs.workflow ~/Library/Services/
+    cp -r "$PWD/Rename PDFs.workflow" ~/Library/Services/
 fi
