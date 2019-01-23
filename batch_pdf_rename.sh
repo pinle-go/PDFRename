781f2462653011079becdfadd52a9c1e7d4e175c
Hanpeng Liu <lhp.polo@gmail.com>
2019-01-22 19:07:38 -0800

    fix bug

--- batch_pdf_rename.sh
+++ batch_pdf_rename.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/python -c 'import mochi' 2>1 >/dev/null || pip install git+https://github.com/pinle-go/PDFRename.git --user >/dev/null
+/usr/bin/python -c 'import mochi' 2>1 >/dev/null || /usr/bin/python -m pip install git+https://github.com/pinle-go/PDFRename.git --user >/dev/null
 /usr/bin/python -m mochi "$@"
